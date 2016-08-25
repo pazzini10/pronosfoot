84bcda8ba9d597d82ebe8a498b76e4c45585b9d6
pazzini10 <david.flouriot@gmail.com>
2016-08-25 17:55:36 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,25 +1,4 @@
-packages<-function(x){
-  x<-as.character(match.call()[[2]])
-  if (!require(x,character.only=TRUE)){
-    install.packages(pkgs=x,repos="http://cran.r-project.org")
-    require(x,character.only=TRUE)
-  }
-}
-packages(shiny)
-packages(formattable)
-packages(plyr)
-packages(reshape2)
-packages(DT)
-packages(datasets)
 
-
-library(shiny)
-library(formattable)
-library(plyr) 
-library(reshape2)
-library(DT)
-library(datasets)
-source("Calcul1.R")
 
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
